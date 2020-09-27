dd7d0749abb0efe7adb65efa114a1ed1fe4895f5
Clark Brown <clark@neighbor.com>
2020-09-27 09:19:10 -0600

    custom favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,27 @@
 module.exports = {
   siteMetadata: {
-    name: `Hello, my name is Clark Brown.`,
-    tagline: `This is my website!`
-  },  
+    siteUrl: `https://clark-brown.web.app`,
+    index: {
+      default: 'Clark Brown'
+    }
+  },
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/images/favicon.png`,
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-eslint`,
