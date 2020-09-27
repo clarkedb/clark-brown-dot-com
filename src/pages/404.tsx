90226c82f734dc41f9adc4b99764fa2bfb561068
Clark Brown <clark@neighbor.com>
2020-09-27 09:32:49 -0600

    page titles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@material-ui/core';
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 import * as styles from './Index.module.scss';
 
 export default class ErrorPage extends Component {
@@ -7,8 +9,16 @@ export default class ErrorPage extends Component {
 
     return (
       <div className={styles.Container}>
+        <Helmet>
+          <title>Page Not Found | Clark Brown</title>
+        </Helmet>
         <h1>Oops!</h1>
         <p>Looks like that page doesn't exist...</p>
+        <Button
+          variant="contained"
+          color="primary"
+          href="/"
+        >Take Me Home</Button>
       </div>
     );
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { graphql } from 'gatsby';
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 import * as styles from './Index.module.scss';
 
 export interface IndexPageProps {
@@ -31,6 +32,9 @@ export default class IndexPage extends Component<IndexPageProps, {}> {
   render() {
     return (
       <div className={styles.Container}>
+        <Helmet>
+          <title>Clark Brown</title>
+        </Helmet>
         <h1>My name is Clark Brown.</h1>
         <p>This is my website.</p>
       </div>
