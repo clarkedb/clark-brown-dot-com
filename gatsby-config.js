83c7899e427a05d582aebbbb216a09f28973b3a6
Clark Brown <clark@neighbor.com>
2020-11-08 09:14:05 -0700

    new stack configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,66 @@
 module.exports = {
   siteMetadata: {
-    name: `Hello, my name is Clark Brown.`,
-    tagline: `This is my website!`
+    title: `Clark Brown Portfolio`,
+    description: `Clark Brown is a developer, mathematician, and problem solver.`,
+    author: `Clark Brown`
   },  
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-eslint`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-tailwindcss`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/data`
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 768,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-personal-website-starter`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#81e6d9`,
+        theme_color: `#81e6d9`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/gatsby-icon.png`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly: [`src/assets/styles/global.css`]
+      }
+    }
   ],
 }
